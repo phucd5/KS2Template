122a5077a5c566d50c6c41d8bf49c220a4ca432b
phucd5 <79371837+phucd5@users.noreply.github.com>
2022-06-01 15:15:40 -0400

    Fixes

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# KS2Template
+# Setup
+
+Install the program with:
+
+```
+git clone https://github.com/phucd5/KS2Template
+```
+
+Install SpikeInterface:
+
+```
+cd ks2template
+pip install -e ./spikeinterface
+```
+
+__Note__: If there are errors make sure the following dependencies are installed_:
+
+- numpy
+- neo>=0.9.0
+- joblib
+- probeinterface
+- tqdm
+- scipy
+- h5py
+- pandas
+- sklearn
+- matplotlib
+- networkx
+- datalad
+- MEArec
+
+
+# Template Requirements
+
+- __U__: low-rank components of the spatial masks for each template
+
+
+- __W__: low-rank components of the temporal masks for each template
+
+
+- __mu__: mean amplitude for each template
+
+This needs to be stored in a struct named ‘rez’ with variables U, W, mu. 
+
+__Note__: If you want use templates founded by a previous run of Kilosort just use the outputted rez folder as the template file 
+
+# Initializing Templates
+
+- Step 1: Enter the directory path to the spike interface folder 
+- Step 2: Enter “Yes” when asked if you want to initialize Kilosort 2 with templates
+- Step 3: Enter the file name for the template
+- Step 4: Enter the directory path to the folder that the template is in
+
+Example:
+
+```
+Enter directory path to spikeinterface folder: /gpfs/ysm/project/seo/phd24/KS2Template/spikeinterface
+Do you want to initialize KS2 with previously found templates? (Yes/No): Yes
+Enter the file name for the template: rez2.mat
+Enter the directory path to the folder of the template: /gpfs/ysm/project/seo/phd24/KS2Template             
+```
+
+__Note__: If you enter "No" for the initialization of templates, then Kilosort will run normally
+
+# Running Kilosort
+
+
+```
+Do you want to use your own dataset? (Yes/No): Yes
+Enter the directory path to the data: /gpfs/ysm/project/seo/phd24/sort/sub-MEAREC-250neuron-Neuropixels_ecephys.nwb
+```
+
+__Note__: If you enter no, Kilosort will generate a dataset for you to use to test the program. 
--- test.py
+++ test.py
@@ -20,6 +20,49 @@ import spikeinterface.comparison as sc
 import spikeinterface.widgets as sw
 from spikeinterface.comparison import GroundTruthStudy
 
+
+#Set the templates for MATLAB
+def init_temp():
+
+    temp_file_name = input("Enter the file name for the template: ")
+    temp_dir = input("Enter the directory path to the folder of the template: ")
+
+    temp_fres = "\nFileName = " + "\'" + temp_file_name + "\';"
+    temp_dirres = "\nFolderName = " + "\'" + temp_dir + "\';"
+
+
+    #/gpfs/ysm/project/seo/phd24/sort/spikeinterface
+    #rez2.mat
+    #/gpfs/ysm/project/seo/phd24/sort/Kilosort
+
+    with open(spike_path, 'a') as input_file:
+        input_file.write(temp_fres)
+        input_file.write(temp_dirres)
+
+def init_data():
+    #Use real dataset
+    data_filename = input("Enter the directory path to the data: ")
+    SX_gt = se.NwbSortingExtractor(str(data_filename))
+    RX = se.NwbRecordingExtractor(str(data_filename))
+    
+    study_folder = 'study_folder'
+    gt_dict = {'rec0' : (RX, SX_gt) }
+    study = GroundTruthStudy.create(study_folder, gt_dict)
+    spike_matching_run(study)
+
+
+def init_test_data():
+    #Create sample data
+    rec0, gt_sorting0 = se.toy_example(num_channels=4, duration=10, seed=5, num_segments=1)
+    gt_dict = {
+    'rec0': (rec0, gt_sorting0),
+    }
+    study_folder = 'study_folder'
+    study = GroundTruthStudy.create(study_folder, gt_dict)
+
+    spike_matching_run(study)
+
+
 #Do we want to run with initalized templates?
 def use_temp():
     run_temp = input("Do you want to initalize KS2 with prviously found templates? (Yes/No): ")
@@ -34,33 +77,20 @@ def use_temp():
     with open(spike_path, 'a') as input_file:
         input_file.write(run_temp_res)
 
-#Set the templates for MATLAB
-def init_temp():
 
-    temp_file_name = input("Enter the file name for the template: ")
-    temp_dir = input("Enter the directory path for the template: ")
+def choose_data():
+    use_data = input("Do you want to use your own dataset? (Yes/No) ")
 
-    temp_fres = "\nFileName = " + "\'" + temp_file_name + "\';"
-    temp_dirres = "\nFolderName = " + "\'" + temp_dir + "\';"
+    use_data = use_data.lower()
 
-
-    #/gpfs/ysm/project/seo/phd24/sort/spikeinterface
-    #rez2.mat
-    #/gpfs/ysm/project/seo/phd24/sort/Kilosort
-
-    with open(spike_path, 'a') as input_file:
-        input_file.write(temp_fres)
-        input_file.write(temp_dirres)
+    if use_data == 'yes':
+        init_data()
+    else:
+        print("Using test data")
+        init_test_data()
 
 
-def run_matching():
-    rec0, gt_sorting0 = se.toy_example(num_channels=4, duration=10, seed=5,
-num_segments=1)
-    gt_dict = {
-    'rec0': (rec0, gt_sorting0),
-    }
-    study_folder = 'a_study_folder'
-    study = GroundTruthStudy.create(study_folder, gt_dict)
+def spike_matching_run(study):
     sorter_list = ['kilosort2']
     study.run_sorters(sorter_list, mode_if_folder_exists="keep")
     study.run_comparisons(exhaustive_gt=True)
@@ -70,14 +100,10 @@ num_segments=1)
         print(rec_name, sorter_name)
         print(comp.count_score)  # raw counting of tp/fp/...
         comp.print_summary()
-        perf_unit = comp.get_performance(method='by_unit')
-        perf_avg = comp.get_performance(method='pooled_with_average')
-        m = comp.get_confusion_matrix()
-        w_comp = sw.plot_agreement_matrix(comp)
-        w_comp.ax.set_title(rec_name  + ' - ' + sorter_name)
+
 
 
 spike_path = input("Enter directory path to spikeinterface folder ")
 spike_path += '/spikeinterface/sorters/kilosort2/kilosort2_config.m'
 use_temp()
-run_matching()
+choose_data()
